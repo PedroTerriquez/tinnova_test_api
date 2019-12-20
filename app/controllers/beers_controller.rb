2845805f979cc83c0fd2b4cb99fcf1da1af95d40
Pedro Terriquez <pedroterriquez@hotmail.com>
2019-12-20 10:54:36 -0600

    Feature: Model and Controller files

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -6,4 +6,28 @@ class BeersController < ApplicationController
         hello: @current_user.name
     }
   end
+
+  def beers
+    response = Faraday.get 'https://api.punkapi.com/v2/beers'
+    binding.pry
+    # Ask for all beers:
+    # id
+    #name
+    #tagline
+    #description
+    #abv
+  end
+
+  def beer_by_name
+
+  end
+
+  def beer_by_abv
+
+  end
+
+  def save_favorite(id)
+    Beer.where(favorite: true).update(favorite: false)
+    Beer.find(id).update(favorite: true)
+  end
 end
---
+++ app/models/beer.rb
@@ -0,0 +1,4 @@
+class Beer < ApplicationRecord
+  #default_scope
+  belongs_to :user
+end
