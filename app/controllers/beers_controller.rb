9212665a5067a178aa9b652b6d120bfab2f8ac37
Pedro Terriquez <pedroterriquez@hotmail.com>
2019-12-20 12:46:29 -0600

    Feature: Getting beers from API and routes working properly

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -3,31 +3,58 @@ class BeersController < ApplicationController
 
   def index
     render json: {
-        hello: @current_user.name
+      hello: @current_user.name
     }
   end
 
-  def beers
-    response = Faraday.get 'https://api.punkapi.com/v2/beers'
-    binding.pry
-    # Ask for all beers:
-    # id
-    #name
-    #tagline
-    #description
-    #abv
+  def all
+    get_and_save_beers
+    render json: @api_beers
   end
 
-  def beer_by_name
-
+  def beers_by_name
+    return if params[:name].empty?
+    req_params = { beer_name: params[:name] }
+    get_and_save_beers(req_params)
   end
 
-  def beer_by_abv
-
+  def beers_by_abv(abv)
+    return if params[:name].empty?
+    req_params = { beer_name: params[:name] }
+    get_and_save_beers(req_params)
   end
 
   def save_favorite(id)
     Beer.where(favorite: true).update(favorite: false)
     Beer.find(id).update(favorite: true)
+  end
+
+  private
+
+  def api
+    @conn ||= Faraday.new(
+      url: 'https://api.punkapi.com/v2',
+      headers: {'Content-Type' => 'application/json'}
+
+    )
+  end
+
+  def get_and_save_beers(req_params = nil)
+    @api_beers = JSON.parse(api.get('beers', req_params).body)
+    save_beers_database
+  end
+
+  def save_beers_database
+    return true
+    @api_beers.each do |beer|
+      Beer.create(
+        id: beer['id'],
+        name: beer['name'],
+        tagline: beer['tagline'],
+        description: beer['description'],
+        abv: beer['abv'],
+        seen_at: DateTime.now
+      )
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'auth/login', to: 'authentication#login'
 
-  resources :beers
+  resources :beers do
+    collection do
+      get :all
+      post :beers_by_name
+      post :beers_by_abv
+      post :save_favorite
+    end
+  end
 end
